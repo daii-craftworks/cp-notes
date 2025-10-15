22d4dbda3cc45af488853770ec0257e64d7c425b
daii-craftworks <eguchi2015i@gmail.com>
2025-10-15 11:03:42 +0900

    chore(templates): add AtCoder I/O fast templates and utils

---
+++ templates/atcoder/io_fast_pypy.py
@@ -0,0 +1,13 @@
+# AtCoder I/O Template (PyPy)
+import sys
+input = sys.stdin.readline
+
+def main():
+    # 例:
+    # n = int(input())
+    # a = list(map(int, input().split()))
+    # TODO: 実装
+    pass
+
+if __name__ == "__main__":
+    main()
